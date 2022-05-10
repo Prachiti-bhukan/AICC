7111fafc23c2ea7b0d0d8e585b510885302b9388
Prachiti bhukan <prachitibhukan@gmail.com>
2022-05-10 19:16:21 +0530

    nqueens problem using branch and bound algo

---
+++ nqueen.java
@@ -0,0 +1,42 @@
+import java.io.* ;
+import java.util.* ;
+
+public class nqueen{
+    public static void solve(boolean[][] board, int row,boolean[] cols, boolean[] ndig, boolean[] rdig, String asf)
+    {
+        if(board.length == row)
+        {
+            System.out.println(asf+".");
+            return;
+        }
+
+        for(int col=0;col<board[0].length;col++)
+        {
+            if(cols[col]==false && ndig[row+col]==false && rdig[row-col+(board.length-1)]==false)
+            {
+                board[row][col]= true;
+                cols[col]= true;
+                rdig[row-col+(board.length-1)]= true;
+                ndig[row+col] = true;
+                solve(board,row+1,cols,ndig,rdig,asf+row+"-"+col+",");
+                board[row][col]= false;
+                cols[col]= false;
+                rdig[row-col+(board.length-1)]= false;
+                ndig[row+col] = false;
+            }
+        }
+
+    }
+ public static void main(String[] args) throws Exception
+ {
+     Scanner scn= new Scanner(System.in);
+     int n= scn.nextInt();
+     boolean[][] board = new boolean[n][n];
+     boolean[] cols = new boolean[n];
+     boolean[] ndig = new boolean[2*n-1];
+     boolean[] rdig = new boolean[2*n-1];
+
+     solve(board,0,cols,ndig,rdig,"");
+
+ }
+}
